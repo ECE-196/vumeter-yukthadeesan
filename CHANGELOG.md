a9efe1605b85795c89d3de18cc2b5f134b988175
yukthadeesan <ydeesan@ucsd.edu>
2024-04-26 22:28:03 -0700

    Submission

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,14 +1,15 @@
 # CHANGELOG
 
-## {DATE}
+## 04-25-2024
 ### Contributors
-...
+Yuktha Deesan
 
 ### Added
-...
+Submission.mov in the repository
 
 ### Changed
-...
+main.py
+
 
 ### Removed
 ...
--- main.py
+++ main.py
@@ -11,27 +11,45 @@ status.direction = Direction.OUTPUT
 
 led_pins = [
     board.IO21,
-    board.IO26, # type: ignore
+    board.IO26, #type:ignore
     board.IO47,
-    # do the rest...
+    board.IO33, #type:ignore
+    board.IO34, #type:ignore
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39,
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
 
+vold_thresholds = [20000, 25000, 30000, 35000, 40000, 45000, 50000, 60000, 70000, 80000, 900000]
+
 for led in leds:
     led.direction = Direction.OUTPUT
 
-# main loop
+
+filtered_volume = 0
+smoothing_factor = 0.7
+
 while True:
     volume = microphone.value
-
+    
     print(volume)
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    if volume > filtered_volume:
+        filtered_volume = volume
+    else:
+        filtered_volume = smoothing_factor*filtered_volume+(1-smoothing_factor)*volume
 
-    sleep(1)
+    led_light = 0
+    for i, t in enumerate(vold_thresholds):
+        if filtered_volume >= t:
+            led_light = i 
 
-    # instead of blinking,
-    # how can you make the LEDs
-    # turn on like a volume meter?
+    for i, led in enumerate(leds):
+        led.value = i < led_light
+
+    sleep(0.1)
